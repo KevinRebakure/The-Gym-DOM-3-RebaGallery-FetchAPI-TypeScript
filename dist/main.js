77199f70cf9ddbc72401324ad3313dd00f00e9c1
Rebakure <kevinrebakure@gmail.com>
2024-07-05 22:37:41 +0200

    done ✅ - Did the date

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,44 @@
 "use strict";
 const generate = document.getElementById("generate");
 const cards = document.getElementById("cards");
+const month = document.getElementById("month");
+const day = document.getElementById("day");
+const year = document.getElementById("year");
+const exp = document.getElementById("exp");
+const fullDate = new Date();
+const date = fullDate.getDate().toString();
+const prefix = {
+    "1": "st",
+    "2": "nd",
+    "3": "rd",
+    "21": "st",
+    "22": "nd",
+    "23": "rd",
+    "31": "st",
+};
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+year.innerText = fullDate.getFullYear().toString();
+month.innerText = months[fullDate.getMonth()];
+day.innerText = date;
+if (Object.keys(prefix).includes(date)) {
+    exp.innerText = prefix[date];
+}
+else {
+    exp.innerText = "th";
+}
 let page = 1;
 async function generateGallery() {
     const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=100`);
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,60 @@
 const generate = document.getElementById("generate");
-const cards:HTMLElement|null = document.getElementById("cards");
+const cards = document.getElementById("cards");
+const month = document.getElementById("month");
+const day = document.getElementById("day");
+const year = document.getElementById("year");
+const exp = document.getElementById("exp");
 
-let page = 1
-async function generateGallery(): Promise<void> {
+const fullDate = new Date();
+const date = fullDate.getDate().toString();
+const prefix: { [key: string]: string } = {
+  "1": "st",
+  "2": "nd",
+  "3": "rd",
+  "21": "st",
+  "22": "nd",
+  "23": "rd",
+  "31": "st",
+};
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+year!.innerText = fullDate.getFullYear().toString();
+month!.innerText = months[fullDate.getMonth()];
+day!.innerText = date;
+
+if (Object.keys(prefix).includes(date)) {
+  exp!.innerText = prefix[date];
+} else {
+  exp!.innerText = "th";
+}
+
+let page = 1;
+async function generateGallery(){
   // Request
   const response = await fetch(
     `https://picsum.photos/v2/list?page=${page}&limit=100`,
   );
   const data = await response.json();
   const randomOrder = data.sort(() => 0.5 - Math.random());
-  page++
-  console.log(page)
+  page++;
+  console.log(page);
 
   // Populate the UI
-  const images = randomOrder.map((image: any):string => {
+  const images = randomOrder.map((image: any): string => {
     const img = document.createElement("img");
     img.src = image.download_url;
     img.className = "w-full aspect-square";
