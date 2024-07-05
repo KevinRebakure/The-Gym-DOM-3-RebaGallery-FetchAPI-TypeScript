bb05076e1f1c17abc7ce7c8827b5f473db413bde
Rebakure <kevinrebakure@gmail.com>
2024-07-05 20:42:10 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainBeige: "#FBF6E2",
+        mainDark: "#131842",
+        mainOrange: "#E68369",
+        customOrange: "#FF7F3E",
+        customBlueish: "#80C4E9",
+        customPurple: "#604CC3",
+        customYellowish: "#FFDE95",
+        customGreen: "#ADD899",
+        customRed: "#BC5A94",
+        customPink: "#F075AA",
+        customDarkBlue: "#131842",
+        customLightYellow: "#F9E2AF",
+        customLightYellow: "#F9E2AF",
+      },
+    },
   },
   plugins: [],
 };
